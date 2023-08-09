e41fb3b003e8c225f37f09b7194904555a60daa1
AkiMira <akimiratest@gmail.com>
2023-08-09 19:55:10 +0300

    09/08/19:55

--- HW1/src/Main.java
+++ HW1/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        String str[] = {"3", "2", "1", "Поехали!!!"};
+        for (int i=0; i < 4; i++){
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println( str[i] );
+        }
+    }
 }
