e73f3e00486c6ed8b74c92cfcf5926cf227f9df9
AkiMira <akimiratest@gmail.com>
2023-08-12 20:53:36 +0300

    12/08

---
+++ JAVA/HW2/Smile.java
@@ -0,0 +1,8 @@
+package JAVA.HW2;
+
+public class Smile {
+  public static void main(String[] args) {
+    System.out.print(Character.toChars(0x1F349));
+    System.out.println(Character.toChars(0x1F63B));
+  }
+}
