46aea247f32dcd537cfd765bafe5eb04d20c29b8
AkiMira <akimiratest@gmail.com>
2023-08-21 16:29:33 +0300

    21/08

--- JAVA/HW4/Task8.java
+++ JAVA/HW4/Task8.java
@@ -7,5 +7,20 @@
 package JAVA.HW4;
 
 public class Task8 {
-  
+  public static void main(String[] args) {
+    int [] array = {1, -1, -2, -2, 3, -3, 0};
+    int sumpositive = 0;
+    int sumnegative = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] > 0) {
+        sumpositive += array[i];
+        // System.out.println(sumpositive);
+      }
+      else {
+        sumnegative += array[i];
+        // System.out.println(sumnegative);
+      }
+    }
+    System.out.println("Сумма положительных чисел массива = " + sumpositive + " Сумма отрицательных чисел массива = " + sumnegative);
+  }
 }
