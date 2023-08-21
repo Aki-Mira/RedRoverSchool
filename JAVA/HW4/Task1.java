c3df4713a41ce37e629c5464ceaabee0f1809105
AkiMira <akimiratest@gmail.com>
2023-08-21 12:04:10 +0300

    21/08

---
+++ JAVA/HW4/Task1.java
@@ -0,0 +1,16 @@
+// DECRIPTION:
+// Необходимо вывести числа от 0 до 15.
+// SOLUTION:
+package JAVA.HW4;
+/**
+ * Task1
+ */
+public class Task1 {
+public static void main(String[] args) {
+int n = 0;
+  for  (int i = 0; i <= 15; i++) {
+    System.out.print(n + " ");
+    n++;
+}
+}
+}
