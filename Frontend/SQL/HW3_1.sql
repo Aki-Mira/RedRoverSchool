342e65f55529d3f4d663e84d91f63514622c641e
AkiMira <akimiratest@gmail.com>
2023-09-06 19:29:32 +0300

    06/09

---
+++ Frontend/SQL/HW3_1.sql
@@ -0,0 +1,9 @@
+-- ALTER TABLE mywork.EMPLOYEE_TEST drop column country;
+
+-- alter table mywork.dept rename column loc to city; 
+
+-- insert into mywork.dept values (5,'HR','ST LOUIS');
+-- insert into mywork.dept values (6,'Engineering','NEW YORK');
+-- insert into mywork.dept values (7,'Marketing','ATLANTA');
+
+-- SELECT * FROM mywork.dept WHERE city IN ('ATLANTA');
---
+++ Frontend/SQL/HW3_2.sql
@@ -0,0 +1,8 @@
+-- SELECT first_name FROM library_simple.author WHERE last_name IN ('Swanson');
+-- Kirk
+
+-- SELECT page_num FROM library_simple.book WHERE `name` IN ('Men Without Fear');
+-- 439
+
+-- SELECT * FROM library_simple.category WHERE `name` LIKE ('W%');
+-- 7 categories
