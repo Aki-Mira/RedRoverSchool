3ab2b93f0d0a55d36d6ffbfb421114f13ee806f4
AkiMira <akimiratest@gmail.com>
2023-09-06 12:37:36 +0300

    06/09

---
+++ JAVA/Remove String Spaces/src/Main.java
@@ -0,0 +1,20 @@
+//DESCRIPTION:
+//        Write a function that removes the spaces from the string, then return the resultant string.
+//
+//        Examples:
+//
+//        Input -> Output
+//        "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+//        "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+//        "8aaaaa dddd r     " -> "8aaaaaddddr"
+//SOLUTION:
+public class Main {
+    public static void main(String[] args) {
+String string = "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd";
+System.out.println(noSpace(string));
+    }
+    public static String noSpace(final String x) {
+        String [] words = x.split(" ");
+        return String.join("", words);
+    }
+}
